a846714ebb748e5136b351ebc5ffabbc80ed3561
hornto <krishnadas10.official@gmail.com>
2023-12-27 00:34:48 +0530

    Category section completed

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,7 +5,10 @@ import { useRouter } from "next/navigation"
 const Logo = () => {
    const router = useRouter() ;
    return (
-    <Image alt = 'Logo' className="hidden md:block cursor-pointer" height='100' width='100'
+
+    <Image
+    onClick={()=>router.push('/')}
+    alt = 'Logo' className="hidden md:block cursor-pointer" height='100' width='100'
     src= '/images/logo.png'/>
    )
 }
